ffd69f432a56fd843732667071c92fb19db9fc65
J031Fm <joey.felip11@gmail.com>
2021-06-17 18:45:40 -0500

    first commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,3 +1,4 @@
+import { escape } from 'querystring';
 import { getRepository, Repository } from 'typeorm';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
@@ -14,17 +15,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = this.repository.findOneOrFail(user_id,{
+      relations: ["games"]
+    })
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+
+    return this.repository.query("select * from users ORDER BY first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    return this.repository.query(
+      `SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2`,
+      [first_name, last_name]);
+
   }
 }
